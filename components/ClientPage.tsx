7120ecbcd79dd7e325a01b89422a2b2aef969c7e
Earnwithalee7890 <earnwithalee@gmail.com>
2026-01-27 02:22:00 +0500

    style: Add global page transition overlay

--- components/ClientPage.tsx
+++ components/ClientPage.tsx
@@ -300,6 +300,7 @@ export default function ClientPage() {
 
                 <Footer setActiveTab={handleTabChange} />
             </div>
+            <div className="page-transition-overlay"></div>
         </>
 
     );
